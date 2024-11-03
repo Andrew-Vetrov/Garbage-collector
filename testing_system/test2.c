8de58ea0df9766a135cc293ba73116790b4a9ca6
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-11-03 21:56:02 +0700

    green background added if success and valgrind check

---
+++ testing_system/test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	void* a = malloc(12313);
+	void* a = calloc(1, 1);
+	return 0;
+}
--- testing_system/testing_system.c
+++ testing_system/testing_system.c
@@ -2,11 +2,14 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fnmatch.h>
 
 int main() {
 	DIR* directory;
 	struct dirent* inp;
 	char command[BUFSIZ] = "gcc ";
+	char* matched_names[BUFSIZ];
+	int match_counter = 0;
 	directory = opendir(".");
 
 	while (1) {
@@ -14,18 +17,19 @@ int main() {
 		if (inp == NULL) {
 			break;
 		}
-		if (strcmp(inp->d_name, "testing_system.c") == 0
-			|| strcmp(inp->d_name, ".") == 0
-			|| strcmp(inp->d_name, "..") == 0
-			|| strchr(inp -> d_name, '.') == NULL) {
-			continue;
+		if (fnmatch("*.c", inp->d_name, 0) == 0 && strcmp(inp->d_name, "testing_system.c") != 0) {
+			matched_names[match_counter++] = inp->d_name;
 		}
-		printf("Testing %s\n", inp->d_name);
-		strncat(command, inp->d_name, strlen(inp->d_name) + 1);
+	}
+	for (int i = 0; i < match_counter; i++) {
+
+		printf("Testing %s\n", matched_names[i]);
+		strncat(command, matched_names[i], strlen(matched_names[i]) + 1);
 		strncat(command, " -o test", 9);
+		printf("\033[1;42mCompiled successfuly\033[0m\n");
 		system(command);
-		system("./test");
-		break;
+		system("valgrind --leak-check=full --leak-resolution=med ./test");
+		
 	}
 	return 0;
 }
