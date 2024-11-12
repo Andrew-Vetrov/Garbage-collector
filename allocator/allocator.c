ce32700cef4123ad4d615282f3b1020bd804fe2e
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-11-12 14:03:05 +0700

    just added vars to header and c file

--- allocator/allocator.c
+++ allocator/allocator.c
@@ -12,21 +12,21 @@ typedef struct Node_t {
 } Node;
 
 size_t START_ALLOCATOR_HEAP = 0;
-size_t END_ALLOCATOR_HEAP;
 size_t end_rsp_value;
+size_t END_ALLOCATOR_HEAP;
 static Node NODES_LIST[HEAP_SIZE / BLOCK_SIZE];
-static Node* SEGREG_LIST[MAX_OBJECT_SIZE] = { 0 };
+static Node* SEGREG_LIST[MAX_OBJECT_SIZE] = {0};
 static Node* EMPTY_LIST_HEAD = 0;
 
 void init_header(Node* entry, size_t object_size) {
 	size_t object_size_header = entry->start_allocator_ptr + 8;
-	*(size_t*)object_size_header = object_size;
-
+	*(size_t *)object_size_header = object_size;
+	
 	size_t bitmap_start = object_size_header + 8;
-	*(size_t*)bitmap_start = 0;
+	*(size_t *)bitmap_start = 0;
 	for (int j = 0; j < 7; j++) {
 		bitmap_start += 8;
-		*(size_t*)bitmap_start = 0;
+		*(size_t *)bitmap_start = 0;
 	}
 }
 
@@ -36,11 +36,11 @@ void fill_all_bitmaps_with_zeros() {
 		current_entry = SEGREG_LIST[i];
 		while (current_entry != NULL) {
 			size_t bitmap_start = current_entry->start_allocator_ptr + 16;
-
-			*(size_t*)bitmap_start = 0;
+			
+			*(size_t *)bitmap_start = 0;
 			for (int j = 0; j < 7; j++) {
 				bitmap_start += 8;
-				*(size_t*)bitmap_start = 0;
+				*(size_t *)bitmap_start = 0;
 			}
 
 			current_entry = current_entry->next_node;
@@ -50,13 +50,12 @@ void fill_all_bitmaps_with_zeros() {
 
 int get_bit_by_address(size_t object_address) {
 	size_t relative_address = object_address - START_ALLOCATOR_HEAP;
-
-	size_t block_start = relative_address - (relative_address % BLOCK_SIZE);
+	size_t block_start = object_address - (relative_address % BLOCK_SIZE);
 	size_t objects_start = block_start + BLOCK_HEADER_SIZE;
 	size_t bitmap_start = block_start + 16;
 
-	int object_size = *(size_t*)(block_start + 8);
-	int addition = (object_size % 8 == 0) ? 0 : 8 - (object_size % 8);
+	size_t object_size = *(size_t *)(block_start + 8);
+	size_t addition = (object_size % 8 == 0) ? 0 : 8 - (object_size % 8);
 	object_size += addition;
 
 	int object_index = (object_address - objects_start) / object_size;
@@ -64,18 +63,17 @@ int get_bit_by_address(size_t object_address) {
 	size_t byte_position = bitmap_start + (object_index / 8);
 	int bit_position = object_index % 8;
 
-	return ((*(unsigned char*)byte_position) >> bit_position) & 1;
+	return ((*(unsigned char *)byte_position) >> bit_position) & 1;
 }
 
 void set_bit_by_address(size_t object_address, int bit) {
 	size_t relative_address = object_address - START_ALLOCATOR_HEAP;
-
-	size_t block_start = relative_address - (relative_address % BLOCK_SIZE);
+	size_t block_start = object_address - (relative_address % BLOCK_SIZE);
 	size_t objects_start = block_start + BLOCK_HEADER_SIZE;
 	size_t bitmap_start = block_start + 16;
 
-	int object_size = *(size_t*)(block_start + 8);
-	int addition = (object_size % 8 == 0) ? 0 : 8 - (object_size % 8);
+	size_t object_size = *(size_t *)(block_start + 8);
+	size_t addition = (object_size % 8 == 0) ? 0 : 8 - (object_size % 8);
 	object_size += addition;
 
 	int object_index = (object_address - objects_start) / object_size;
@@ -83,57 +81,52 @@ void set_bit_by_address(size_t object_address, int bit) {
 	size_t byte_position = bitmap_start + (object_index / 8);
 	int bit_position = object_index % 8;
 
-	if ((((*(unsigned char*)byte_position) >> bit_position) & 1) != bit) {
-		*(unsigned char*)byte_position = (*(unsigned char*)byte_position) ^ (1 << bit_position);
+	if ((((*(unsigned char *)byte_position) >> bit_position) & 1) != bit) {
+		*(unsigned char *)byte_position = (*(unsigned char *)byte_position) ^ (1 << bit_position);
 	}
 }
 
 size_t get_object_size_by_address(size_t object_address) {
 	size_t relative_address = object_address - START_ALLOCATOR_HEAP;
-	size_t block_start = relative_address - (relative_address % BLOCK_SIZE);
-
-	size_t object_size = *(size_t*)(block_start + 8);
-
-	return object_size;
+	size_t block_start = object_address - (relative_address % BLOCK_SIZE);
+	size_t object_size = *(size_t *)(block_start + 8);
+    
+    return object_size;
 }
 
 __attribute__((constructor))
 void init_allocator() {
 	START_ALLOCATOR_HEAP =
-		(size_t)mmap(NULL, HEAP_SIZE,
-			PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-
+		(size_t) mmap(NULL, HEAP_SIZE,
+				PROT_WRITE | PROT_READ,MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+	
 	if (START_ALLOCATOR_HEAP == MAP_FAILED) {
 		fprintf(stderr, "Can't allocate allocator's heap!\n");
 		return;
 	}
-
 	END_ALLOCATOR_HEAP = START_ALLOCATOR_HEAP + HEAP_SIZE;
-
 	int nodes_count = HEAP_SIZE / BLOCK_SIZE;
 
 	for (int i = 0; i < nodes_count; i++) {
-		NODES_LIST[i].start_allocator_ptr =
+		NODES_LIST[i].start_allocator_ptr = 
 			START_ALLOCATOR_HEAP + BLOCK_SIZE * i;
-		*(size_t*)NODES_LIST[i].start_allocator_ptr =
+		*(size_t*)NODES_LIST[i].start_allocator_ptr = 
 			NODES_LIST[i].start_allocator_ptr + BLOCK_HEADER_SIZE;
 		if (i != nodes_count - 1) {
 			NODES_LIST[i].next_node = &NODES_LIST[i + 1];
-		}
-		else {
+		} else {
 			NODES_LIST[i].next_node = 0;
 		}
 	}
 
-	EMPTY_LIST_HEAD = &NODES_LIST[0];
+    EMPTY_LIST_HEAD = &NODES_LIST[0];
 }
 
 Node* allocate_new_block() {
 	if (EMPTY_LIST_HEAD == NULL) {
 		fprintf(stderr, "No empty blocks in garbage collector!\n");
 		return NULL;
-	}
-	else {
+	} else {
 		Node* result = EMPTY_LIST_HEAD;
 		EMPTY_LIST_HEAD = EMPTY_LIST_HEAD->next_node;
 		result->next_node = NULL;
@@ -143,7 +136,7 @@ Node* allocate_new_block() {
 
 __attribute__((destructor))
 void destroy_allocator() {
-	if (munmap((void*)START_ALLOCATOR_HEAP, HEAP_SIZE) == -1) {
+	if (munmap((void*) START_ALLOCATOR_HEAP, HEAP_SIZE) == -1) {
 		fprintf(stderr, "Can't unmap heap!\n");
 	}
 }
@@ -153,10 +146,9 @@ size_t allocate_new_object(size_t object_size) {
 		fprintf(stderr, "Size of object is too large\n");
 		return NULL;
 	}
-	//asm volatile("mov %%rsp, %0" : "=r" (end_rsp_value));
-	printf("13123");
+	asm volatile("mov %%rsp, %0" : "=r" (end_rsp_value));
 	Node* current_entry = SEGREG_LIST[object_size];
-	Node* prev_entry;
+	Node *prev_entry;
 
 	size_t addition = (object_size % 8 == 0) ? 0 : 8 - (object_size % 8);
 	size_t block_start;
@@ -171,29 +163,27 @@ size_t allocate_new_object(size_t object_size) {
 
 			// put your code here :))
 
-		}
-		else {
+		} else {
 			init_header(current_entry, object_size);
 		}
 	}
 
 	while (current_entry != NULL) {
 		block_start = current_entry->start_allocator_ptr;
-		free_space_address = *(size_t*)block_start;
+		free_space_address = *(size_t *)block_start;
 		next_block_start = block_start + BLOCK_SIZE;
 
 		while (free_space_address + object_size + addition <= next_block_start) {
 			if (get_bit_by_address(free_space_address) == 0) {
-				*(size_t*)block_start = free_space_address + object_size + addition;
+				*(size_t *)block_start = free_space_address + object_size + addition;
 				return free_space_address;
-			}
-			else {
+			} else {
 				set_bit_by_address(free_space_address, 0);
 				free_space_address = free_space_address + object_size + addition;
 			}
 		}
 
-		*(size_t*)block_start = free_space_address;
+		*(size_t *)block_start = free_space_address;
 
 		prev_entry = current_entry;
 		current_entry->next_node;
@@ -208,16 +198,15 @@ size_t allocate_new_object(size_t object_size) {
 
 		// put your code here :))
 
-	}
-	else {
+	} else {
 		current_entry = current_entry->next_node;
 
 		init_header(current_entry, object_size);
 
 		block_start = current_entry->start_allocator_ptr;
-		free_space_address = *(size_t*)block_start;
+		free_space_address = *(size_t *)block_start;
 
-		*(size_t*)block_start = free_space_address + object_size + addition;
+		*(size_t *)block_start = free_space_address + object_size + addition;
 		return free_space_address;
 	}
 }
--- testing_system/test.c
+++ testing_system/test.c
@@ -9,9 +9,7 @@ int main() {
 	//A* c = (A*)allocate_new_object(sizeof(A));
 	//A* b = (A*)allocate_new_object(sizeof(A));
 	//printf("%p\n", &a);
-	printf("%llu %llu\n", start_rsp_value, end_rsp_value);
 	int* a = allocate_new_object(40);
-	printf("%llu %llu\n", start_rsp_value, end_rsp_value);
 	segment_traverse(end_rsp_value, start_rsp_value);
 	return 0;
 }
