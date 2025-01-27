bfd76e5f9d152ccdbc926403328d1f63665eb62b
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2025-01-27 23:53:09 +0700

    Try to help CI detect errors while testing

--- testing_system/testing_system.c
+++ testing_system/testing_system.c
@@ -50,16 +50,18 @@ int main() {
 			waitpid(pid, &status, 0);
 			if (WIFSIGNALED(status)) {
 				printf("\033[1;43mTime limit exceed\033[0m\n");
+				return 1;
 				continue;
 			}
 			if (WEXITSTATUS(status) == 0) {
 				printf("\033[1;42mExecuted successfuly\033[0m\n");
+				return 1;
 				continue;
 			}
 			printf("\033[1;41mAn error occured while runtime\033[0m\n");
 		}
 	}
-	system("rm testing_system/test");
+	//system("rm ./testing_system/test");
 	closedir(directory);
 	return 0;
 }
