11781724eafa657f3ae5352fdce94b3458801e96
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2025-02-17 13:28:50 +0700

    Fix testing system

--- testing_system/testing_system.c
+++ testing_system/testing_system.c
@@ -16,7 +16,7 @@ int main() {
 	int compilation_result;
 	int runtime_result;
 	directory = opendir("./testing_system");
-	char OBJ[] = "./allocator/allocator.o ./scanner/marking.o ./scanner/stack.o";
+	char OBJ[] = " ./allocator/allocator.o ./scanner/marking.o ./scanner/stack.o";
 	while (1) {
 		inp = readdir(directory);
 		if (inp == NULL) {
@@ -30,7 +30,7 @@ int main() {
 			match_counter++;
 		}
 	}
-	
+	printf("CONTER %d\n", match_counter);
 	for (int i = 0; i < match_counter; i++) {
 		char command[BUFSIZ] = "gcc ";
 		printf("\nTesting %s\n", matched_names[i]);
@@ -38,11 +38,10 @@ int main() {
 		if (pid == 0) {
 			char command[BUFSIZ] = "gcc ";
 			strncat(command, matched_names[i], strlen(matched_names[i]) + 1);
-
+			//strncat(command, OBJ, strlen(OBJ));
+			strncat(command, " -L./ -l:lib.a", 22);
 			strncat(command, " -o test ", 9);
-			strncat(command, OBJ, strlen(OBJ));
-			strncat(command, " -L./ -l:./lib.a", 22);
-
+			printf("Command %s\n", command);
 			compilation_result = system(command);
 			if (compilation_result == 256) {
 				printf("\033[1;41mCompilation failed\033[0m\n");
@@ -57,12 +56,10 @@ int main() {
 			waitpid(pid, &status, 0);
 			if (WIFSIGNALED(status)) {
 				printf("\033[1;43mTime limit exceed\033[0m\n");
-				return 1;
 				continue;
 			}
 			if (WEXITSTATUS(status) == 0) {
 				printf("\033[1;42mExecuted successfuly\033[0m\n");
-				return 1;
 				continue;
 			}
 			printf("\033[1;41mAn error occured while runtime\033[0m\n");
@@ -70,5 +67,6 @@ int main() {
 	}
 	//system("rm ./testing_system/test");
 	closedir(directory);
+	printf("ENDED SUCCESSFULLY\n");
 	return 0;
 }
