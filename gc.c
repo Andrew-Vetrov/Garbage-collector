f6f0ae18bcdceaafe5a7260a5fa63c7d3b4cbefe
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-29 05:50:07 +0300

    calloc + realloc without gc_free() function

--- gc.c
+++ gc.c
@@ -1,8 +1,10 @@
 #include "allocator/allocator.h"
+#include "allocator/object.h"
 #include "allocator/utils.h"
 #include "logging/log.h"
 #include "marker/marking.h"
 #include "sweeper/sweep.h"
+#include <string.h>
 
 size_t gc_malloc(size_t size) {
     if (size < 1 || size > HEAP_SIZE) {
@@ -16,5 +18,24 @@ size_t gc_malloc(size_t size) {
         sweep();
         res = allocate_new_object(size);
     }
+    return res;
+}
+
+size_t gc_calloc(size_t size) {
+    size_t res = gc_malloc(size);
+    if (res != NULL) {
+        memset((void*) res, 0, size);
+    }
+
+    return res;
+}
+
+size_t gc_realloc(void* memblock, size_t size) {
+    size_t res = gc_malloc(size);
+    if (res != NULL) {
+        memcpy((void*) res, memblock, get_object_size((Object) memblock));
+        //gc_free(memblock); // to do
+    }
+
     return res;
 }
